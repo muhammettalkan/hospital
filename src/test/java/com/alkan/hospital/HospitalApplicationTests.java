ea325952d6a413733f92c566612372c260276260
xulame <alkan.muhammet.47@gmail.com>
2023-10-03 13:09:55 +0300

    first commit

---
+++ src/test/java/com/alkan/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkan.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
